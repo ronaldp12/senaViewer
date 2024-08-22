e15c308d6e29e2929323a3e420ac75288cb7b334
Ronald patiño <patinoguevararonald@gmail.com>
2024-08-21 20:26:00 -0500

    funcion serie y revista lista

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import edu.misena.senaviewer.model.Book;
-import edu.misena.senaviewer.model.Chapter;
-import edu.misena.senaviewer.model.Movie;
+import edu.misena.senaviewer.model.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,6 +10,8 @@ public class Main {
     private static ArrayList<Book> libros = new ArrayList<>();
     private static ArrayList<Chapter> capitulos = new ArrayList<>();
     private static ArrayList<Movie> peliculas = new ArrayList<>();
+    private static ArrayList<Serie> series = new ArrayList<>();
+    private static ArrayList<Magazine> revistas = new ArrayList<>();
 
     public static void main(String[] args) throws ParseException {
         inicio();
@@ -92,6 +92,36 @@ public class Main {
 
         Movie pelicula=new Movie(title,genre,creator,duration);
         peliculas.add(pelicula);
+    }
+
+    public static void addSerie(Scanner scanner){
+        System.out.println("Ingrese el título de la serie:");
+        String title = scanner.nextLine();
+
+        System.out.println("Ingrese el genero de la serie:");
+        String genre = scanner.nextLine();
+
+        System.out.println("Ingrese la duracion de la serie:");
+        int duration = scanner.nextInt();
+
+        Serie serie=new Serie(title,genre,duration);
+        series.add(serie);
+    }
+
+    public static void addMagazine(Scanner scanner) throws ParseException {
+        System.out.println("Ingrese el título de la revista:");
+        String title = scanner.nextLine();
+
+        System.out.println("Ingrese la fecha de edicion de la revista:");
+        String fecha = scanner.nextLine();
+        SimpleDateFormat convertir = new SimpleDateFormat();
+        Date edititionDate= convertir.parse(fecha);
+
+        System.out.println("Ingrese la editorial de la revista:");
+        String editorial = scanner.nextLine();
+
+        Magazine revista=new Magazine(title,edititionDate,editorial);
+        revistas.add(revista);
     }
 
 
